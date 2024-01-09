bdf7597509e3aa48d9b0fcf7f48ae9533d62a8e1
juliscrep <julietascrepnik@gmail.com>
2024-01-09 16:02:18 -0300

    se implementa cache

--- app/flaskr/routes.py
+++ app/flaskr/routes.py
@@ -6,33 +6,58 @@ from microservices import db
 from microservices import logger
 from microservices import predictor
 from datetime import datetime
+from cachetools import TTLCache
+import logging
+
+# Configura el nivel de registro (DEBUG, INFO, etc.)
+logging.basicConfig(level=logging.DEBUG)
+
+
+CACHE_SIZE = 100
+CACHE_EXPIRATION_TIME = 60  # segundos
+cache = TTLCache(maxsize=CACHE_SIZE, ttl=CACHE_EXPIRATION_TIME)
 
 @app.route('/predict', methods=['GET'])
 def predict():
+    try: 
+        start_time = datetime.now().timestamp()
 
-    start_time = datetime.now().timestamp()
+        authentication_result, user_info = authenticator.authenticate(request, start_time)
+    
+        if authentication_result:
 
-    authentication_result, user_info = authenticator.authenticate(request, start_time)
-    # TODO: Authenticate QUOTA (authenticator.authenticate)
-    if authentication_result:
-        # TODO: Implementar Cache
+            params = {
+                "colesterol": float(request.args.get("colesterol", 0)),
+                "presion": float(request.args.get("presion", 0)),
+                "glucosa": float(request.args.get("glucosa", 0)),
+                "edad": float(request.args.get("edad", 0)),
+                "sobrepeso": float(request.args.get("sobrepeso", 0)),
+                "tabaquismo": float(request.args.get("tabaquismo", 0))
+             }
+        
+            cache_key = json.dumps(params)
+            
+            if cache_key in cache:
+                result = cache[cache_key]
+                #print("Usando la caché disponible:", cache_key)
+                logging.debug("Usando la caché disponible: %s", cache_key)
+            else:
+            
+                json_data = json.dumps(params)
+                result = predictor.predict(json_data)
 
-        params = {
-            "colesterol": float(request.args.get("colesterol", 0)),
-            "presion": float(request.args.get("presion", 0)),
-            "glucosa": float(request.args.get("glucosa", 0)),
-            "edad": float(request.args.get("edad", 0)),
-            "sobrepeso": float(request.args.get("sobrepeso", 0)),
-            "tabaquismo": float(request.args.get("tabaquismo", 0))
-        }
+                cache[cache_key] = result
 
-        json_data = json.dumps(params)
+                logger.log(json_data, result, start_time, user_info)
+                #print("Nueva caché disponible:", cache_key)
+                logging.debug("Nueva caché disponible: %s", cache_key)
 
-        # Perform prediction
-        result = predictor.predict(json_data)
+        else:
+            result = {"respuesta": "Error de autenticación"}, 403
 
-        logger.log(json_data, result, start_time, user_info)
-    else:
-        result = {"respuesta": "Error de autenticación"}, 403
+        return result
 
-    return result
+    except ValueError as e:
+        return {"Error en la función predict al colocar un valor incorrecto": str(e)}, 400
+    except Exception as e:
+        return {"Error en la función predict": str(e)}, 500
